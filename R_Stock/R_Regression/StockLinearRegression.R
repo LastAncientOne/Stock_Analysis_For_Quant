ca6ebeb8b9cf63855926dfeda6c5a92e8626a46f
LastAncientOne <lastancientone@gmail.com>
2021-07-12 17:58:12 -0700

    Add files via upload

---
+++ R_Stock/R_Regression/StockLinearRegression.R
@@ -0,0 +1,21 @@
+# Linear Regression
+
+library(quantmod)
+
+# Pull data from Yahoo finance 
+getSymbols('AAPL', from='2016-01-01', to='2018-01-01', adjust = TRUE)
+Stock <- AAPL
+Stock = data.frame(AAPL)
+Stock$Date = as.Date(rownames(Stock))
+
+# independent variable belongs on the x-axis (horizontal line) of the graph 
+# dependent variable belongs on the y-axis (vertical line) of the graph
+# X - Independent Variable
+# Y - Dependent Variable
+x <- Stock$AAPL.Open
+y <- Stock$AAPL.Adjusted
+
+# Linear regression attempts to model the relationship between two variables 
+# by fitting a linear equation to observed data
+relationship <- lm(y~x)
+print(relationship)
