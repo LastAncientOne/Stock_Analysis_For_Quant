94ed51fe56dd8bb5775faf1f492ca6360279ee5b
LastAncientOne <lastancientone@gmail.com>
2021-07-20 06:00:22 -0700

    Add files via upload

---
+++ R_Stock/R_Regression/StockDecisionTree.R
@@ -0,0 +1,29 @@
+# Decision Tree Regression
+
+library(quantmod)
+library(modelr)
+library(rpart)
+
+# Pull data from Yahoo finance 
+getSymbols('AAPL', from='2016-01-01', to='2018-01-01', adjust = TRUE)
+Stock <- AAPL
+Stock = data.frame(AAPL)
+Stock$Date = as.Date(rownames(Stock))
+
+x <- as.matrix(Stock[c(1:3,5)])
+y <- Stock$AAPL.Adjusted
+
+model <- rpart(x ~ y, 
+             method = "anova", data = Stock )
+
+data.frame(
+  R2 = rsquare(model, data = Stock),
+  RMSE = rmse(model, data = Stock),
+  MAE = mae(model, data = Stock)
+)
+
+plot(model, uniform = TRUE,
+     main = "Stock Decision Tree using Regression")
+text(model, use.n = TRUE, cex = .6)
+
+print(model)
