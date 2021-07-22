7204ae006b2effc97e76ef4f2636a42c894b0228
LastAncientOne <lastancientone@gmail.com>
2021-07-22 05:19:27 -0700

    Add files via upload

---
+++ R_Stock/R_Regression/StockQuantileRegression.R
@@ -0,0 +1,37 @@
+# Quantile Regression
+
+library(quantmod)
+library(quantreg)
+library(broom)
+library(modelr)
+library(dplyr)
+library(ggplot2)
+library(caret)
+
+# Pull data from Yahoo finance 
+getSymbols('AAPL', from='2016-01-01', to='2018-01-01', adjust = TRUE)
+Stock <- AAPL
+Stock = data.frame(AAPL)
+Stock$Date = as.Date(rownames(Stock))
+
+head(Stock)
+
+tail(Stock)
+
+qr <- glm(AAPL.Adjusted ~ ., data = Stock, family = poisson)
+
+print(qr)
+
+glance(qr)
+
+data.frame(
+  R2 = rsquare(qr, data = Stock),
+  RMSE = rmse(qr, data = Stock),
+  MAE = mae(qr, data = Stock)
+)
+
+summary(qr)
+
+plot(AAPL.Adjusted ~ AAPL.Open , data = Stock, pch = 16, main = "Plot")
+abline(lm(AAPL.Adjusted ~ AAPL.Open , data = Stock), col = "red", lty = 2)
+abline(rq(AAPL.Adjusted ~ AAPL.Open , data = Stock), col = "blue", lty = 2)
