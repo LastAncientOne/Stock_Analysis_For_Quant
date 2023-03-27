671b777cb7c4b97604a7101a5da905d9ad4678a1
LastAncientOne <lastancientone@gmail.com>
2023-03-27 07:55:50 -0700

    Update Sortino_Ratio.R

--- R_Stock/R_Risk_Returns_Ratios/Sortino_Ratio.R
+++ R_Stock/R_Risk_Returns_Ratios/Sortino_Ratio.R
@@ -22,6 +22,7 @@ print(symbol)
 print(sortino_ratio)
 cat(symbol, ': ', sortino_ratio)
 
+
 # Multiple Stock symbols
 # Define the stock symbols of the library
 tickers <- c("BAC", "GS", "C", "WFC", "JPM")
