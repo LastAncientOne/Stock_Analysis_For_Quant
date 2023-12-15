7eb2f2d41a0f5366ecd641bbfe1539f113171a65
LastAncientOne <lastancientone@gmail.com>
2023-12-15 07:10:01 -0800

    Add files via upload

---
+++ R_Stock/R_Forecast/R_Forecast_HETAF.R
@@ -0,0 +1,41 @@
+# Theta method forecast
+# Load required libraries
+library(quantmod)
+library(forecast)
+library(ggplot2)
+
+# Define your symbol and time frame
+symbol <- 'NVDA'
+start <- '2018-01-01'
+end <- '2023-08-01'
+
+# Download stock data
+getSymbols(symbol, from = start, to = end)
+stock_data <- Ad(get(symbol))
+
+# Convert stock data to a time series (assuming monthly data)
+stock_ts <- ts(stock_data, frequency = 12)
+
+# Perform forecasting using Theta method (thetaf)
+theta_model <- thetaf(stock_ts)
+
+# Define the forecast period
+forecast_period <- 12  # Example: forecasting for the next 12 periods (adjust as needed)
+
+# Forecast using thetaf
+forecast_result <- forecast(theta_model, h = forecast_period)
+
+# Plot the forecast
+plot(forecast_result, main = paste(symbol, " Stock Price Forecast"))
+
+# Add historical data in blue
+lines(stock_ts, col = "blue", lwd = 2)
+
+# Print the forecast result
+print(forecast_result)
+
+# Calculate accuracy measures for the forecast
+accuracy_measures <- accuracy(forecast_result)
+
+# Print the accuracy measures
+print(accuracy_measures)
