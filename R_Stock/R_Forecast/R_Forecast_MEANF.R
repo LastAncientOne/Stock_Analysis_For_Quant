115637fc4ffa2ecac94746cce1213e4dcef9f201
LastAncientOne <lastancientone@gmail.com>
2023-12-18 07:26:49 -0800

    Add files via upload

---
+++ R_Stock/R_Forecast/R_Forecast_MEANF.R
@@ -0,0 +1,61 @@
+# Mean Forecast
+# Load required libraries
+library(quantmod)
+library(forecast)
+library(ggplot2)
+
+# Define your symbol and time frame
+symbol <- 'NVDA'
+start <- '2018-01-01'
+end <- '2023-08-01'
+
+# Download stock data
+getSymbols(symbol, from = start, to = end)
+stock_data <- Ad(get(symbol))
+
+# Convert stock data to a time series (assuming monthly data)
+stock_ts <- ts(stock_data, frequency = 12)
+
+# Define the forecast period
+forecast_period <- 12  # Example: forecasting for the next 12 periods (adjust as needed)
+
+# Create a sequence of dates for the forecast period
+forecast_dates <- seq(as.Date(end), length.out = forecast_period + 1, by = "months")[2:(forecast_period + 1)]
+
+# Fit a cubic spline to historical data
+spline_fit <- smooth.spline(time(stock_ts), stock_ts, spar = 0.5)
+
+# Predict using the cubic spline model for the forecast period
+forecast_values <- predict(spline_fit, x = as.numeric(forecast_dates))$y
+
+# Convert the forecasted values to a time series
+forecast_ts <- ts(forecast_values, frequency = 12)
+
+# Fit a Mean Forecast model to the historical data
+mean_forecast <- meanf(stock_ts, h = forecast_period)
+
+# Plot the forecast
+plot(mean_forecast, main = paste(symbol, " Stock Price Forecast"), xlab = "Date", ylab = "Price", type = "l", col = "red")
+lines(stock_ts, col = "blue", lwd = 2)
+
+# Add a legend
+legend("topright", legend = c("Mean Forecast", "Historical"), col = c("red", "blue"), lty = 1, lwd = 2)
+
+# Print the forecast result
+print(mean_forecast$mean)
+
+# Calculate forecast accuracy measures
+accuracy(mean_forecast$mean, stock_ts)
+
+
+# Add a legend
+legend("topright", legend = c("Forecast", "Historical"), col = c("red", "blue"), lty = 1, lwd = 2)
+
+# Print the forecast result
+print(forecast_ts)
+
+# Print the forecast result
+print(mean_forecast$mean)
+
+# Calculate forecast accuracy measures
+accuracy(mean_forecast)
