5c0b94ef30ad2233dcdfec592b98e97ca0b82de4
LastAncientOne <lastancientone@gmail.com>
2023-12-30 17:11:17 -0800

    Add files via upload

---
+++ R_Stock/R_Forecast/R_smooth_es.R
@@ -0,0 +1,32 @@
+# sim.es() and simulate() - simulation functions for Exponential Smoothing.
+# Load the required libraries
+library(quantmod)
+library(forecast)
+
+# Define your stock symbol and date range
+symbol <- "NVDA"  # Change this to your desired stock symbol
+start <- "2020-01-01"
+end <- "2023-10-11"
+
+# Download stock data
+getSymbols(symbol, from = start, to = end)
+
+# Extract the stock price data
+stock_data <- Ad(get(symbol))
+
+# Perform Exponential Smoothing
+# You can choose your desired smoothing parameters (alpha, beta, gamma) and method
+alpha <- 0.2  # Smoothing parameter (adjust as needed)
+beta <- NULL  # Smoothing parameter (set to NULL for simple exponential smoothing)
+gamma <- NULL  # Smoothing parameter (set to NULL for simple exponential smoothing)
+method <- "AAN"  # Exponential Smoothing method (AAN for Additive Error, Additive Trend, Additive Seasonality)
+
+# Fit the exponential smoothing model
+es_model <- ets(stock_data, model = "AAN", alpha = alpha, beta = beta, gamma = gamma)
+
+# Plot the results
+plot(forecast(es_model))
+
+# Generate a forecast for a specific number of periods (e.g., 10 periods)
+forecast_values <- forecast(es_model, h = 10)
+print(forecast_values)
