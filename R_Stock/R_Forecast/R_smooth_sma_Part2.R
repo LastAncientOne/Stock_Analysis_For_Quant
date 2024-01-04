76a7d0de27714b90e291368a49a9ea5ece2537b2
LastAncientOne <lastancientone@gmail.com>
2024-01-04 06:51:36 -0800

    Add files via upload

---
+++ R_Stock/R_Forecast/R_smooth_sma_Part2.R
@@ -0,0 +1,17 @@
+# Load the required libraries
+library(quantmod)
+library(smooth)
+
+# Define your stock symbol and date range
+symbol <- "NVDA"  # Change this to your desired stock symbol
+start <- "2020-01-01"
+end <- "2023-10-11"
+
+# Download stock data
+getSymbols(symbol, from = start, to = end)
+
+# Extract the stock price data
+stock_data <- Ad(get(symbol))
+
+sma(stock_data, h=18, silent=FALSE)
+
