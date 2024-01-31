7d5debd48be582dad0a5f08585a0db60409834c3
LastAncientOne <lastancientone@gmail.com>
2024-01-31 07:55:10 -0800

    Add files via upload

---
+++ R_Stock/R_Forecast/R_TSA_arima_Part2.R
@@ -0,0 +1,30 @@
+# Load necessary libraries
+library(quantmod)
+library(TSA)# install.packages("TSA", type = "binary")
+library(forecast)
+
+# Define your stock symbol and date range
+symbol <- "NVDA"  # Change this to your desired stock symbol
+start <- as.Date("2020-01-01")
+end <- as.Date("2024-01-01")
+
+# Download stock data
+getSymbols(symbol, from = start, to = end, auto.assign = TRUE)
+
+# Extract the closing prices from the stock data
+closing_prices <- Cl(get(symbol))
+
+# Convert returns to time series
+closing_ts <- ts(closing_prices, start = start, frequency = 252) 
+
+# Fit an ARIMA model
+arima_model <- Arima(closing_ts)
+
+# Print the ARIMA model summary
+summary(arima_model)
+
+# Forecast future returns
+forecast_values <- forecast(arima_model, h = 10)  # You can adjust the 'h' parameter as needed
+
+# Print the forecast
+print(forecast_values)
