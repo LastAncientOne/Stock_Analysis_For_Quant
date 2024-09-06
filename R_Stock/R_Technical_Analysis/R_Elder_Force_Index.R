6d5e00bd6b3608d4c682c536851e70fc18fa6c29
LastAncientOne <lastancientone@gmail.com>
2024-09-06 07:27:35 -0700

    Add files via upload

---
+++ R_Stock/R_Technical_Analysis/R_Elder_Force_Index.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(gridExtra)
+library(quantmod)
+
+# Get stock data
+getSymbols('AAPL', from = '2016-01-01', to = '2018-01-01', adjust = TRUE)
+Stock <- AAPL
+
+# Calculate Elder Force Index (EFI)
+Stock$EFI <- (Cl(Stock) - lag(Cl(Stock))) * Vo(Stock)
+
+# Create a ggplot chart for Closing Price
+closing_price_chart <- ggplot(Stock, aes(x = index(Stock), y = AAPL.Close)) +
+  geom_line() +
+  labs(title = "AAPL Closing Price",
+       x = "Date",
+       y = "Price") +
+  theme(plot.title = element_text(hjust = 0.5))  # Center the plot title
+
+# Create a ggplot chart for Elder Force Index (EFI)
+efi_chart <- ggplot(Stock, aes(x = index(Stock), y = EFI)) +
+  geom_line(color = 'blue') +
+  labs(title = "Elder Force Index",
+       x = "Date",
+       y = "EFI") +
+  theme(plot.title = element_text(hjust = 0.5))  # Center the plot title
+
+# Arrange charts using grid
+grid.arrange(closing_price_chart, efi_chart, ncol = 1)
